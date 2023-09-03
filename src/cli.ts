bf7baa6a25254beb9214d390e2e08d07c303f3c3
heliandro <heliandro.l@gmail.com>
2023-09-03 03:57:22 -0300

    fix(cli): corrige os erros do cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
-import GenerateKeyPair from './application/usecase/GenerateKeyPair';
-import EncryptionAlgorithm from './domain/value-object/EncryptionAlgorithm';
-import CryptoAdapterImpl from './infra/adapters/CryptoAdapterImpl';
+import GenerateKeyPair from '@app/application/usecase/GenerateKeyPair';
+import GetKey from '@app/application/usecase/GetKey';
+import CryptoRepositoryFileSystem from '@app/infra/repository/CryptoRepositoryFileSystem';
 
 // driver, primary driver, input adapter
 process.stdin.on("data", async function (chunk) {
@@ -9,36 +9,46 @@ process.stdin.on("data", async function (chunk) {
     if (command.startsWith("help")) {
         console.log("\nList of commands: \n");
         console.table({
-            "generate-keys": "generate public and private keys",
-            encrypt: "encrypt data. Ex: encrypt \"{ name: 'Heliandro' }\""
+            "generate-keys": "generate public and private keys. Exec: > generate-keys",
+            "get-key": "get public or private key. Exec: > get-key public",
+            // encrypt: "encrypt data. Exec: > encrypt \"{ name: 'Heliandro' }\""
         })
     }
 
     if (command.startsWith("generate-keys")) {
-        const encryptionAlgorithmVo = new EncryptionAlgorithm();
-        const cryptoAdapter = new CryptoAdapterImpl(encryptionAlgorithmVo)
-        const usecase = new GenerateKeyPair(cryptoAdapter);
+        const repository = new CryptoRepositoryFileSystem();
+        const usecase = new GenerateKeyPair(repository);
         const output = await usecase.execute()
         console.log(output);
     }
 
-    if (command.startsWith("encrypt")) {
-        const data = command.replace(/encrypt\s{0,1}/, "")
+    if (command.startsWith("get-key")) {
+        const data: KeyType = <KeyType>command.replace(/get-key\s{0,1}/, "")
+        const input: { keyType: KeyType } = { keyType: data }
+
+        const repository = new CryptoRepositoryFileSystem();
+        const usecase = new GetKey(repository);
+        const output = await usecase.execute(input)
+        console.log(output);
+    }
+
+    // if (command.startsWith("encrypt")) {
+    //     const data = command.replace(/encrypt\s{0,1}/, "")
         
-        if (!data) {
-            console.error('Data not found!')
-            return;
-        }
+    //     if (!data) {
+    //         console.error('Data not found!')
+    //         return;
+    //     }
 
-        try {
-            const parsedData = JSON.parse(data)
-            console.log(parsedData)
-        } catch (error: any) {
-            console.error("Incorrete JSON format. ", error?.message)
-        }
-    }
+    //     try {
+    //         const parsedData = JSON.parse(data)
+    //         console.log(parsedData)
+    //     } catch (error: any) {
+    //         console.error("Incorrete JSON format. ", error?.message)
+    //     }
+    // }
 
-    if (command.startsWith("decrypt")) {
-        console.log("TODO - decrypt data....")
-    }
+    // if (command.startsWith("decrypt")) {
+    //     console.log("TODO - decrypt data....")
+    // }
 })
