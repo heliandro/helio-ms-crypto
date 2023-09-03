0712e773204a8cd76f052aa32caddc46333d9db7
heliandro <heliandro.l@gmail.com>
2023-09-03 12:15:20 -0300

    chore: aplicar configuracoes no jest e package.json

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
-    '^@app/(.*)$': '<rootDir>/src/$1'
+    '^@app/(.*)$': '<rootDir>/src/$1',
+    '^@tests/(.*)$': '<rootDir>/tests/$1'
   }
 };
