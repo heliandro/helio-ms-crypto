2ba0d2c414ff925d841c6664ada3cd65f7e82f6d
heliandro <heliandro.l@gmail.com>
2023-09-03 17:50:48 -0300

    tests: aplicar aumento da cobertura dos testes

---
+++ tests/unit/EncryptionAlgorithm.test.ts
@@ -0,0 +1,35 @@
+import EncryptionAlgorithm from "@app/domain/value-object/EncryptionAlgorithm"
+import { RSAKeyPairOptions } from "node:crypto"
+
+const mockDefaultOptions: RSAKeyPairOptions<'pem', 'pem'> = {
+    modulusLength: 2048,
+    publicKeyEncoding: { 
+        type: 'spki',
+        format: 'pem' 
+    },
+    privateKeyEncoding: { 
+        type: 'pkcs8',
+        format: 'pem'
+    }
+}
+
+describe('EncryptionAlgorithm', () => {
+
+    test('Deve criar o algoritmo de encriptacao passando as opcoes', () => {
+        // Given - When
+        const encryptionAlgorithm = new EncryptionAlgorithm('rsa', mockDefaultOptions);
+        // Then
+        expect(encryptionAlgorithm.keyType).toBe('rsa')
+        expect(encryptionAlgorithm.options).toStrictEqual(mockDefaultOptions)
+    })
+
+    test('Deve criar o algoritmo de encriptacao usando o metodo estatico', () => {
+        // Given
+        const expectedOtions = { ...mockDefaultOptions, modulusLength: 1024 }
+        // When
+        const encryptionAlgorithm = EncryptionAlgorithm.create();
+        // Then
+        expect(encryptionAlgorithm.keyType).toBe('rsa')
+        expect(encryptionAlgorithm.options).toStrictEqual(expectedOtions)
+    })
+})
---
+++ tests/unit/KeyPair.test.ts
@@ -0,0 +1,23 @@
+import KeyPair from "@app/domain/entity/KeyPair"
+import { MOCK_PRIVATE_KEY, MOCK_PUBLIC_KEY } from "@tests/utils/KeyPair.constants"
+
+describe('KeyPair', () => {
+
+    test('Deve criar uma instanciar da classe usando uma chave publica válida', () => {
+        const keyPair = new KeyPair(MOCK_PUBLIC_KEY, '')
+        expect(keyPair.publicKey).toBe(MOCK_PUBLIC_KEY)
+    })
+
+    test('Deve criar uma instanciar da classe usando uma chave privada válida', () => {
+        const keyPair = new KeyPair('', MOCK_PRIVATE_KEY)
+        expect(keyPair.privateKey).toBe(MOCK_PRIVATE_KEY)
+    })
+
+    test('Deve lançar um erro ao instanciar a classe com uma chave publica inválida', () => {
+        expect(() => new KeyPair('chave publica inválida', '')).toThrow(new Error(`A(s) chave(s) de criptografia pem são inválidas`))
+    })
+
+    test('Deve lançar um erro ao instanciar a classe com uma chave privada inválida', () => {
+        expect(() => new KeyPair('', 'chave privada inválida')).toThrow(new Error(`A(s) chave(s) de criptografia pem são inválidas`))
+    })
+})
