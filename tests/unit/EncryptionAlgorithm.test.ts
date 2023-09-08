b54c8f8984104b119b0d5a8b4c8f4e7bc07f4ddd
heliandro <heliandro.l@gmail.com>
2023-09-08 17:21:11 -0300

    chore: ajustes no package e algoritmo de encriptacao

--- tests/unit/EncryptionAlgorithm.test.ts
+++ tests/unit/EncryptionAlgorithm.test.ts
@@ -25,7 +25,7 @@ describe('EncryptionAlgorithm', () => {
 
     test('Deve criar o algoritmo de encriptacao usando o metodo estatico', () => {
         // Given
-        const expectedOtions = { ...mockDefaultOptions, modulusLength: 1024 }
+        const expectedOtions = { ...mockDefaultOptions, modulusLength: 2048 }
         // When
         const encryptionAlgorithm = EncryptionAlgorithm.create();
         // Then
